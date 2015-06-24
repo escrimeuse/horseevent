64e1fdcac43951a10ab95448385aef53e6ce5d21
Cathryn Griffiths <cathryn.griffiths@gmail.com>
2015-06-24 16:40:57 -0400

    added show record, add horse, edit details

--- resources/views/yourhorses.blade.php
+++ resources/views/yourhorses.blade.php
@@ -2,6 +2,167 @@
 
 @section('content')
 
+    <!-- Add Horse Modal -->
+    <div class="modal fade bs-example-modal-md" id="add" tabindex="-1" role="dialog" aria-labelledby="addModal">
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Add New Horse</h4>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="horseNameAdd">Horse Name</label>
+                            <input type="text" class="form-control" id="horseName">
+                        </div>
+                        <div class="form-group">
+                            <label for="horsePhotoAdd">Photo</label>
+                            <input type="file" id="horsePhoto">
+                        </div>
+                        <div class="form-group">
+                            <label for="horseSexAdd">Sex</label>
+                            <select class="form-control">
+                                <option>Mare</option>
+                                <option>Gelding</option>
+                                <option>Stallion</option>
+                            </select>
+
+                        </div>
+                        <div class="form-group">
+                            <label for="horseAgeAdd">Age</label>
+                            <input type="text" class="form-control" id="horseAge">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="horseBreedAdd">Breed</label>
+                            <input type="text" class="form-control" id="horseBreed">
+                        </div>
+                        <div class="form-group">
+                            <label for="horseHeightAdd">Height</label>
+                            <input type="text" class="form-control" id="horseHeight">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="horseColourAdd">Colouring</label>
+                            <input type="text" class="form-control" id="horseColour">
+                        </div>
+
+
+
+                    </form>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-default">Add Horse</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close Without Adding</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Edit Horse Details  Modal -->
+    <div class="modal fade bs-example-modal-md" id="edit" tabindex="-1" role="dialog" aria-labelledby="editModal">
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Edit Details for [Horse Name]</h4>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="horseName">Horse Name</label>
+                            <input type="text" class="form-control" id="horseName">
+                        </div>
+                        <div class="form-group">
+                            <label for="horsePhoto">Photo</label>
+                            <input type="file" id="horsePhoto">
+                        </div>
+                        <div class="form-group">
+                            <label for="horseSex">Sex</label>
+                            <select class="form-control">
+                                <option>Mare</option>
+                                <option>Gelding</option>
+                                <option>Stallion</option>
+                            </select>
+
+                        </div>
+                        <div class="form-group">
+                            <label for="horseAge">Age</label>
+                            <input type="text" class="form-control" id="horseAge">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="horseBreed">Breed</label>
+                            <input type="text" class="form-control" id="horseBreed">
+                        </div>
+                        <div class="form-group">
+                            <label for="horseHeight">Height</label>
+                            <input type="text" class="form-control" id="horseHeight">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="horseColour">Colouring</label>
+                            <input type="text" class="form-control" id="horseColour">
+                        </div>
+
+
+
+                    </form>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-default">Save</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close Without Saving</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Show Record Modal -->
+    <div class="modal fade bs-example-modal-lg" id="showRecord" tabindex="-1" role="dialog" aria-labelledby="showRecordModal">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Show Record for [Horse Name]</h4>
+                </div>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Show Date</th>
+                            <th>Show Name</th>
+                            <th>Class</th>
+                            <th>Placement/Result</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>[Show Date]</td>
+                            <td>[Show Name]</td>
+                            <td>[Class]</td>
+                            <td>[Placement/Result]</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <h1>Your Horses</h1>
     <div class="spacer" style="height:30px"></div>
 
@@ -38,19 +199,20 @@
                     </tr>
                 </table>
                 <div class="panel-footer">
-                    <a href=""><button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Edit</button></a>
-                    <a href=""><button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Remove</button></a>
+                    <button class="btn btn-default" type="submit" data-toggle="modal" data-target="#showRecord"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> Show Record</button>
+                    <button class="btn btn-default" type="submit" data-toggle="modal" data-target="#edit"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Edit</button>
+
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <a href="">
-                <button class="btn btn-default" type="submit">
+
+                <button class="btn btn-default" type="submit" data-toggle="modal" data-target="#add">
                     <p></p>
                     <p><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></p>
                     <p>Add Horse</p>
                 </button>
-            </a>
+
         </div>
 
 
